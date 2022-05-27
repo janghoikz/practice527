1b8adb3f30131a12a142026b9c4f3dd65f1766a5
ohne <ghdlr785@gmail.com>
2022-05-27 20:39:18 +0900

    초기 커밋.

---
+++ practice527/practice527.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+void PrintHelloWorld()
+{
+    std::cout << "Hello World!" << std::endl;
+}
+void PrintNumber(int number)
+{
+    std::cout << "넘겨주신 숫자는" << number << "입니다" << std::endl;
+}
+
+int MultiplyBy2(int a)
+{
+    int result = a * 2;
+    return result;
+}
+
+int MultiplyBy(int a, int b)
+{
+    int c = a * b;
+    return c;
+}
+
+int main()
+{
+    int a = 5;
+    int b = 3;
+    int result = MultiplyBy(a, b);
+    PrintNumber(result);
+}
